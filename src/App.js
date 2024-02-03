27a01f22304d26b3b16954e875e3dd5192ab4275
amithkashyap <amithkashyaprv@gmail.com>
2024-02-03 19:09:26 +0530

    refactored App component to add Redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import appStore from "./utils/store/AppStore";
+import SearchMovieList from "./components/SearchMovieList";
+import WatchedMovieList from "./components/WatchedMovieList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="text-9xl">Its Movie Time!</header>
-    </div>
+    <Provider store={appStore}>
+      <div className="w-full">
+        <Navbar />
+        <main className="flex w-[900px] mx-auto">
+          <SearchMovieList />
+          <WatchedMovieList />
+        </main>
+      </div>
+    </Provider>
   );
 }
 
