df8ced5a9066cf6a7ec2a668545f8b00b5ef35a6
amithkashyap <amithkashyaprv@gmail.com>
2024-02-03 19:12:17 +0530

    implemented WatchedMovieList component

---
+++ src/components/WatchedMovieList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MovieDetails from "./MovieDetails";
+
+const WatchedMovieList = () => {
+  return (
+    <div className=" text-white w-1/2  bg-[#2b3035] mx-3">
+      <MovieDetails />
+    </div>
+  );
+};
+
+export default WatchedMovieList;
